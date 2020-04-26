2278aa840b9c0be63779cac31f2dca70b86300df
Ben Haus <ben@little-haus.com>
2020-04-26 16:00:38 -0400

    initial commit with some basic setup

---
+++ comake-4/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>App Header</h1>      
+        </header>
+        <h2>App</h2>
+    </div>
+  );
+}
+
+export default App;

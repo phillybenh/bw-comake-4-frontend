c8d78492183337b2aa7ebd685d18e5ed17520fdb
Emilio <ediaz-goico@me.bergen.edu>
2020-04-28 12:27:12 -0400

    finished main page componenet, however i cant seem to get the validtion working

--- comake-4/src/App.js
+++ comake-4/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import IssueForm from './IssueForm';
+import Issues from './Issues'
 
 function App() {
+  const [issueBoard, setIssueBoard] = useState ([
+    {
+    id: 1,
+    title: 'Potholes',
+    description: 'There are potholes all around our city!'
+  }
+])
+
+const addNewIssue = note => {
+  const newIssue = {
+    id:Date.now(),
+    title: note.title,
+    description: note.description
+  }
+  setIssueBoard([...issueBoard, newIssue])
+}
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>App Header</h1>      
+        <h1>Co-make 4</h1>      
         </header>
-        <h2>App</h2>
+        <h2>Submit New Issue</h2>
+        <IssueForm addNewIssue={addNewIssue}/>
+        <Issues issue={issueBoard} />
     </div>
   );
 }
